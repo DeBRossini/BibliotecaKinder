bf1ccf7932fd18c30386e4425085cd7bfbb4b64c
Débora Rossini <dehrossini@outlook.com>
2025-01-13 14:34:23 -0300

    Versao 1.0

--- BibliotecaKinder/__init__.py
+++ BibliotecaKinder/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
-app.config['SECRET_KEY'] = "f238c63c6df4c9ad67d92056de68f5c6"
+load_dotenv() 
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config["UPLOAD_FOLDER"] = "static/capas"
 
 database = SQLAlchemy(app)
