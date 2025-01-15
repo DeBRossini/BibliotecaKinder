193f8601b109dbe2a2f0f65220896fc034724699
Débora Rossini <dehrossini@outlook.com>
2025-01-15 13:41:45 -0300

    Versao 1.0 - atualização do database

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -7,7 +7,7 @@ from urllib.parse import quote_plus
 
 password = quote_plus("4879@@de")
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{password}@localhost/sys"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{password}@localhost:3306/sys"
 app.config['SECRET_KEY'] = "f238c63c6df4c9ad67d92056de68f5c6"
 app.config["UPLOAD_FOLDER"] = "static/capas"
 
