74019fe020f81697f1dd4242917322bfc634a407
Débora Rossini <dehrossini@outlook.com>
2025-01-19 23:44:11 -0300

    Version 1.0 - atualizado port

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -8,8 +8,8 @@ from urllib.parse import quote_plus
 
 DB_USER = os.getenv("DB_USER", "biblioteca")  
 DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", "4879##de"))
-DB_HOST = os.getenv("DB_HOST", "192.168.1.21") 
-DB_PORT = os.getenv("DB_PORT", "3306") 
+DB_HOST = os.getenv("DB_HOST", "192.168.1.56") 
+DB_PORT = os.getenv("DB_PORT", "50523") 
 DB_NAME = os.getenv("DB_NAME", "sys")
 SECRET_KEY = os.getenv("SECRET_KEY", "f238c63c6df4c9ad67d92046de68f5c6")
 
