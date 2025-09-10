714c674fce863f281e902adf1c96422388a8c057
DeBRossini <dehrossini@outlook.com>
2025-09-10 12:45:14 -0300

    Update __init__.py

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,25 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from urllib.parse import quote_plus
-import pymysql
-
-pymysql.install_as_MySQLdb()
 
 DB_USER = "db_kinder"
 DB_PASSWORD = quote_plus("Loca@102030")
-DB_HOST = "db_kinder.mysql.dbaas.com.br"
-DB_NAME = "db_kinder"
+DB_HOST =  "db_kinder.mysql.dbaas.com.br"
+DB_NAME =  "db_kinder"
 SECRET_KEY = "f238c63c6df4c9ad67d92046de68f5c6"
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}?connect_timeout=30"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'pool_recycle': 300,
-    'pool_pre_ping': True,
-    'pool_timeout': 30,
-    'max_overflow': 10
-}
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
